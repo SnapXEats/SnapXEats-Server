d2f2027ac2b3d14f4fa5a88e688e9c36fcffb5ea
nikeshrathod <niksrathod009@gmail.com>
2018-01-16 17:02:43 +0530

    Implemented get food stack pics base on selected cuisine tiles API
     - Created API document for get get food stack pics base on selected cuisine tiles API

--- server/routes.js
+++ server/routes.js
@@ -7,4 +7,5 @@ const req = require;
 
 module.exports = function (app) {
   app.use('/api/v1/cusine', req('./controllers/v1/cusine'));
+  app.use('/api/v1/dishes', req('./controllers/v1/dishes'));
 };
