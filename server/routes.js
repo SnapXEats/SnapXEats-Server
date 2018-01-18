8edc4cf1bb30a0fbc1d83309064289bb4a6d6102
nikeshrathod <niksrathod009@gmail.com>
2018-01-18 18:18:29 +0530

    Implemented Get user information from FB API
      - Create swagger API document for same

--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,7 @@
 const req = require;
 
 module.exports = function (app) {
-  app.use('/api/v1/cusine', req('./controllers/v1/cusine'));
+  app.use('/api/v1/cuisine', req('./controllers/v1/cusine'));
   app.use('/api/v1/dishes', req('./controllers/v1/dishes'));
+  app.use('/api/v1/users', req('./controllers/v1/users'));
 };
