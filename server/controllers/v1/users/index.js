99cfb27161a310216156801e6269c867d91000f6
nikeshrathod <niksrathod009@gmail.com>
2018-01-31 18:01:52 +0530

    Implemented API for save user address

--- server/controllers/v1/users/index.js
+++ server/controllers/v1/users/index.js
@@ -2,7 +2,9 @@
 
 const controller = require('./users.controller');
 const router = require('express').Router();
+const auth = require('./../../../../lib/auth');
 
 router.post('/', controller.signUp);
+router.post('/address', auth.isAuthenticated(), controller.saveUserAddresses);
 
 module.exports = router;
