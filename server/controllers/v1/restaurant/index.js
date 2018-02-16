d1e3ca1ee6a726bad2d861d1ef854cfaff861956
nikeshrathod <niksrathod009@gmail.com>
2018-02-16 16:25:05 +0530

    Implement Restaurant Details API

--- server/controllers/v1/restaurant/index.js
+++ server/controllers/v1/restaurant/index.js
@@ -4,5 +4,6 @@ const controller = require('./restaurant.controller');
 const router = require('express').Router();
 
 router.get('/:restaurantInfoId', controller.getRestaurantInforamtion);
+router.get('/restaurantDetails/:restaurantInfoId', controller.getRestaurantDetails);
 
 module.exports = router;
