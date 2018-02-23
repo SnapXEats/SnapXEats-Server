575ecf299807e463abc5d51a6afdebfd24aa415d
nikeshrathod <niksrathod009@gmail.com>
2018-02-23 14:46:14 +0530

    Modified get dishes API and also modified API document

--- server/controllers/v1/dishes/dishes.controller.js
+++ server/controllers/v1/dishes/dishes.controller.js
@@ -16,6 +16,8 @@ const sleep = require('system-sleep');
 
 const type = require('type-of-is');
 
+const _ = require('underscore');
+
 db.restaurantInfo.hasMany(db.restaurantDish, {
   foreignKey: 'restaurant_info_id'
 });
@@ -76,18 +78,30 @@ function getPlacesResult(adress, googleIds) {
  * @returns {Object} googleIds - Unique google ids array
  *                   pgtoken   - String (Page token for search next page result)
  */
-function getRestaurant(latitude, longitude, distance, googleIds, pagetoken) {
+function getRestaurant(latitude, longitude, distance, sort_by_distance, googleIds, pagetoken) {
   return co(function* () {
     let adr;
     let pgtoken;
     let result;
     if (pagetoken) {
       sleep(1200);
-      adr = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${distance}&type=restaurant&key=${key}&pagetoken=${pagetoken}`;
-      result = yield getPlacesResult(adr, googleIds);
+      if(sort_by_distance){
+        adr = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?rankby=distance&location=${latitude},${longitude}&radius=${distance}&type=restaurant&key=${key}&pagetoken=${pagetoken}`;
+        result = yield getPlacesResult(adr, googleIds);
+      } else {
+        adr = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${distance}&type=restaurant&key=${key}&pagetoken=${pagetoken}`;
+        result = yield getPlacesResult(adr, googleIds);
+      }
+
     } else {
-      adr = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${distance}&type=restaurant&key=${key}`;
-      result = yield getPlacesResult(adr, googleIds);
+      if(sort_by_distance){
+        adr = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?rankby=distance&location=${latitude},${longitude}&radius=${distance}&type=restaurant&key=${key}`;
+        result = yield getPlacesResult(adr, googleIds);
+      } else {
+        adr = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${distance}&type=restaurant&key=${key}`;
+        result = yield getPlacesResult(adr, googleIds);
+      }
+
     }
     if (result.pgtoken) {
       pgtoken = result.pgtoken;
@@ -106,17 +120,23 @@ function getRestaurant(latitude, longitude, distance, googleIds, pagetoken) {
  *
  * @param {Array} restaurantArray - google Ids array for search restaurant
  * @param {Array}  cusineArray - user preferences base on cuisine selection
+ * @param {Array} foodPreferenceData - user preferences base on food selection
+ * @param {Number} restaurant_rating - rating of restaurant
+ * @param {Number} restaurant_price - price of restaurant
  *
  * @returns {Object} restaurant_info_id - String (restaurant unique id)
  *                   restaurant_name   - String (Name of restaurant)
  *                   restaurantDishes   - Array (Dishes array)
  */
-function findRestaurantData(restaurantArray, cusineArray) {
+function findRestaurantData(restaurantArray,restaurant_rating, restaurant_price,
+                            foodPreferenceData, cusineArray) {
   return co(function* () {
     let restaurantCount = 0;
     const responseArray = [];
     let whereClause = {};
+    let whereClauseForRating, whereClauseForPrice;
     const findFoodLabelArray = [];
+
     if (cusineArray.length > 0) {
       cusineArray.forEach((cuisine) => {
         const labelJson = {
@@ -128,10 +148,34 @@ function findRestaurantData(restaurantArray, cusineArray) {
         $or : findFoodLabelArray
       };
     }
+
+    if (typeof restaurant_rating !== "undefined"){
+      whereClauseForRating = {
+          $gte: parseFloat(restaurant_rating),
+          $lte: parseFloat(restaurant_rating) + 0.9
+        };
+    } else {
+      whereClauseForRating = {
+        $gte: 0,
+        $lte: 5.0
+      };
+    }
+
+    if(typeof restaurant_price !== "undefined"){
+      whereClauseForPrice = restaurant_price;
+    } else {
+      whereClauseForPrice = {
+        $gte: 0,
+        $lte: 5.0
+      }
+    }
+
     for (restaurantCount; restaurantCount < restaurantArray.length; restaurantCount++) {
       const result = yield db.restaurantInfo.find({
         where : {
-          restaurant_google_id : restaurantArray[restaurantCount]
+          restaurant_google_id : restaurantArray[restaurantCount],
+          restaurant_rating : whereClauseForRating,
+          restaurant_price : whereClauseForPrice
         },
         include : [{
           model : db.restaurantDish,
@@ -150,6 +194,7 @@ function findRestaurantData(restaurantArray, cusineArray) {
           location_lat : result.location_lat,
           location_long : result.location_long,
           restaurant_price : result.restaurant_price,
+          restaurant_rating : result.restaurant_rating,
           restaurantDishes : result.restaurantDishes
         });
       }
@@ -237,6 +282,10 @@ function findRestaurantData(restaurantArray, cusineArray) {
  *     summary: List all dishes on base of user preferences
  *     description: List all dishes as an JSON array
  *     parameters:
+ *      - in: header
+ *        name: Authorization
+ *        description: an authorization header (Bearer eyJhbGciOiJI...)
+ *        type: string
  *      - in: query
  *        name: latitude
  *        schema:
@@ -248,6 +297,30 @@ function findRestaurantData(restaurantArray, cusineArray) {
  *          type: number
  *        description: user's longitude of current location
  *      - in: query
+ *        name: restaurant_distance
+ *        schema:
+ *          type: integer
+ *        description: user's distance preferences
+ *      - in: query
+ *        name: restaurant_price
+ *        schema:
+ *          type: integer
+ *        description: user's price preferences
+ *      - in: query
+ *        name: restaurant_rating
+ *        schema:
+ *          type: integer
+ *        description: user's rating preferences
+ *      - in: query
+ *        name: sort_by_rating
+ *        schema:
+ *          type: integer
+ *      - in: query
+ *        name: sort_by_distance
+ *        schema:
+ *          type: integer
+ *        description: user's preferences on rating
+ *      - in: query
  *        name: cuisineArray
  *        schema:
  *          type: string
@@ -257,6 +330,16 @@ function findRestaurantData(restaurantArray, cusineArray) {
  *        schema:
  *          type: string
  *        description: user's cuisine preferences
+ *      - in: query
+ *        name: foodArray
+ *        schema:
+ *          type: string
+ *        description: user's food preferences
+ *      - in: query
+ *        name: foodArray
+ *        schema:
+ *          type: string
+ *        description: user's food preferences
  *     tags:
  *       - Dishes
  *     produces:
@@ -270,21 +353,109 @@ function findRestaurantData(restaurantArray, cusineArray) {
  */
 exports.getDIshes = function (req, res) {
   return co(function* () {
-    const distance = 1610;
     const googleIds = [];
     let cuisineArray = [];
+    let foodPreferenceData = [];
+    let distance = 1610;
+    let restaurant_rating, restaurant_price, sort_by_distance;
+    let sort_by_rating = 0;
+    if(req.decodedData) {
+      let userId = req.decodedData.user_id;
+      let resolvedPromises = yield Promise.all([
+        db.userPreferences.find({
+        where : {
+          user_id : userId
+        },
+        attributes : ['restaurant_rating', 'restaurant_price', 'restaurant_distance',
+          'sort_by_distance', 'sort_by_rating']
+        }),
+        db.userFoodPreferences.find({
+          where : {
+            user_id : userId
+          },
+          attributes : ['food_type_info_id', 'is_food_like', 'is_food_favourite']
+        })
+      ]);
+
+      let preferenceData = resolvedPromises[0];
+      let userFoodPreferenceData = resolvedPromises[1];
+
+      if(preferenceData.restaurant_distance){
+        distance = preferenceData.restaurant_distance * 1610;
+      }
+
+      if(preferenceData.restaurant_rating){
+        restaurant_rating = preferenceData.restaurant_rating;
+      }
+
+      if(preferenceData.restaurant_price){
+        restaurant_price = preferenceData.restaurant_price;
+      }
+
+      if(preferenceData.sort_by_distance){
+        sort_by_distance = preferenceData.sort_by_distance;
+      }
+
+      if(preferenceData.sort_by_rating){
+        sort_by_rating = preferenceData.sort_by_rating;
+      }
+
+      if(userFoodPreferenceData.length > 0){
+        userFoodPreferenceData.forEach((foodObject) => {
+          foodPreferenceData.push(foodObject.food_type_info_id);
+        });
+      }
+
+    } else if(!req.decodedData){
+      if(req.query.restaurant_distance){
+        distance = req.query.restaurant_distance * 1610;
+      }
+
+      if(req.query.restaurant_rating){
+        restaurant_rating = req.query.restaurant_rating;
+      }
+
+      if(req.query.restaurant_price){
+        restaurant_price = req.query.restaurant_price;
+      }
+
+      if(req.query.sort_by_distance){
+        sort_by_distance = req.query.sort_by_distance;
+      }
+
+      if(req.query.sort_by_rating){
+        sort_by_rating = req.query.sort_by_rating;
+      }
+      if (type(req.query.foodArray, String)) {
+        foodPreferenceData.push(req.query.foodArray);
+      } else {
+        foodPreferenceData = req.query.foodArray;
+      }
+    }
+
     if (type(req.query.cuisineArray, String)) {
       cuisineArray.push(req.query.cuisineArray);
     } else {
       cuisineArray = req.query.cuisineArray;
     }
 
-    let data = yield getRestaurant(req.query.latitude, req.query.longitude, distance, googleIds);
+    let data = yield getRestaurant(req.query.latitude, req.query.longitude, distance,
+      sort_by_distance, googleIds);
     if (data.pgtoken) {
       data = yield getRestaurant(req.query.latitude, req.query.longitude,
-        distance, data.googleIds, data.pgtoken);
+        distance, sort_by_distance, data.googleIds, data.pgtoken);
     }
-    return yield findRestaurantData(data.googleIds, cuisineArray);
+    let restaurantData =  yield findRestaurantData(data.googleIds, restaurant_rating, restaurant_price,
+      foodPreferenceData, cuisineArray);
+    if(!sort_by_rating){
+      return restaurantData;
+    } else {
+      let restaurantSortedData = _.sortBy(restaurantData, function(foodObject) {
+        return foodObject.restaurant_rating;
+      });
+      return restaurantSortedData.reverse();
+    }
+
   }).then((data) => {
     res.status(200)
       .json({ dishesInfo : data });
--- server/controllers/v1/dishes/index.js
+++ server/controllers/v1/dishes/index.js
@@ -2,7 +2,8 @@
 
 const controller = require('./dishes.controller');
 const router = require('express').Router();
+const auth = require('./../../../../lib/auth');
 
-router.get('/', controller.getDIshes);
+router.get('/', auth.isAuthenticatedOrNot(), controller.getDIshes);
 
 module.exports = router;
