79c901cae0bad95435ad94155223b7864c812348
nikeshrathod <niksrathod009@gmail.com>
2018-02-23 15:20:16 +0530

    Merge user gestures API
      - Like dish by user API
      - Dislike dish by user API
      - WishList of dish by user API
      - Removed undo dish by user API

--- server/controllers/v1/userGesture/index.js
+++ server/controllers/v1/userGesture/index.js
@@ -4,9 +4,6 @@ const controller = require('./userGesture.controller');
 const router = require('express').Router();
 const auth = require('./../../../../lib/auth');
 
-router.post('/dislike', auth.isAuthenticated(), controller.dislikePictureByUser);
-router.post('/wishList', auth.isAuthenticated(), controller.wishListPictureByUser);
-router.post('/like', auth.isAuthenticated(), controller.pictureLikeByUser);
-router.get('/undo', auth.isAuthenticated(), controller.undoDislikePictureByUser);
+router.post('/', auth.isAuthenticated(), controller.insertUserGestures);
 
 module.exports = router;
--- server/controllers/v1/userGesture/userGesture.controller.js
+++ server/controllers/v1/userGesture/userGesture.controller.js
@@ -14,14 +14,112 @@ db.restaurantDish.belongsTo(db.restaurantInfo, {
 });
 
 /**
+ * insertLikeDishByUser (Insert dish which is like by user)
+ *
+ * @param {string} user_id - user unique id
+ * @param {array}  like_dish_array - dishes like by user
+ *
+ * @returns {object} message
+ */
+
+function insertLikeDishByUser(user_id, like_dish_array) {
+  return co(function* () {
+    let like_dish_count;
+    for(like_dish_count = 0; like_dish_count < like_dish_array.length; like_dish_count++){
+      yield db.userGestures.create({
+        user_id : user_id,
+        restaurant_dish_id : like_dish_array[like_dish_count].restaurant_dish_id,
+        gesture_type : CONSTANTS.USER_GESTURE.LIKE_BY_USER
+      });
+    }
+    if (like_dish_count === like_dish_array.length){
+      return Promise.resolve({
+        message : 'like dishes by user are inserted successfully'
+      });
+    }
+  }).catch((err) => {
+    return err;
+  });
+}
+
+/**
+ * insertDisLikeDishByUser (Insert dish which is dislike by user)
+ *
+ * @param {string} user_id - user unique id
+ * @param {array}  dislike_dish_array - dishes dislike by user
+ *
+ * @returns {object} message
+ */
+
+function insertDisLikeDishByUser(user_id, dislike_dish_array) {
+  return co(function* () {
+    let dislike_dish_count;
+    for(dislike_dish_count = 0; dislike_dish_count < dislike_dish_array.length; dislike_dish_count++){
+      yield db.userGestures.create({
+        user_id : user_id,
+        restaurant_dish_id : dislike_dish_array[dislike_dish_count].restaurant_dish_id,
+        gesture_type : CONSTANTS.USER_GESTURE.DISLIKE_BY_USER
+      });
+    }
+    if (dislike_dish_count === dislike_dish_array.length){
+      return Promise.resolve({
+        message : 'Dislike dishes by user are inserted successfully'
+      });
+    }
+  }).catch((err) => {
+    return err;
+  });
+}
+
+/**
+ * insertWishListDishByUser (Insert dish which is user's wishlist)
+ *
+ * @param {string} user_id - user unique id
+ * @param {array}  wishlist_dish_array - dishes wishlist by user
+ *
+ * @returns {object} message
+ */
+function insertWishListDishByUser(user_id, wishlist_dish_array) {
+  return co(function* () {
+    let wishlist_dish_count;
+    for(wishlist_dish_count = 0; wishlist_dish_count < wishlist_dish_array.length; wishlist_dish_count++){
+      yield db.userGestures.create({
+        user_id : user_id,
+        restaurant_dish_id : wishlist_dish_array[wishlist_dish_count].restaurant_dish_id,
+        gesture_type : CONSTANTS.USER_GESTURE.WISHLIST_OF_USER
+      });
+    }
+    if (wishlist_dish_count === wishlist_dish_array.length){
+      return Promise.resolve({
+        message : 'Wishlist dishes of user are inserted successfully'
+      });
+    }
+  }).catch((err) => {
+    return err;
+  });
+}
+
+/**
+ * @swagger
+ * definition:
+ *   gesture:
+ *     type: object
+ *     required:
+ *       - restaurant_dish_id
+ *     properties:
+ *       restaurant_dish_id:
+ *         type: string
+ */
+
+/**
  * @swagger
  * paths:
- *  /api/v1/userGesture/dislike:
+ *  /api/v1/userGesture:
  *    post:
- *      summary: set dislike dish by user.
+ *      summary: insert user gestures on dish.
  *      tags:
  *        - User Gestures
- *      description: set dislike dish by user as a JSON object
+ *      description: insert user gestures on dish as a JSON object
  *      consumes:
  *        - application/json
  *      parameters:
@@ -35,29 +133,44 @@ db.restaurantDish.belongsTo(db.restaurantInfo, {
  *          description: Set user gestures(dislike).
  *          schema:
  *            type: object
- *            required:
- *              - restaurant_dish_id
  *            properties:
- *              restaurant_dish_id:
- *                type: string
+ *              like_dish_array:
+ *                type: array
+ *                items:
+ *                  $ref: "#/definitions/gesture"
+ *              dislike_dish_array:
+ *                type: array
+ *                items:
+ *                  $ref: "#/definitions/gesture"
+ *              wishlist_dish_array:
+ *                type: array
+ *                items:
+ *                  $ref: "#/definitions/gesture"
  *      responses:
- *        201:
+ *        200:
  *          description: Created
  */
 
-exports.dislikePictureByUser = function (req, res) {
+exports.insertUserGestures = function (req, res) {
 	return co(function* () {
 		let user_id = req.decodedData.user_id;
-		let restaurant_dish_id = req.body.restaurant_dish_id;
+		let like_dish_array = req.body.like_dish_array;
+    let dislike_dish_array = req.body.dislike_dish_array;
+    let wishlist_dish_array = req.body.wishlist_dish_array;
 
-		let createGesture = yield db.userGestures.create({
-      user_id : user_id,
-      restaurant_dish_id : restaurant_dish_id,
-      gesture_type : CONSTANTS.USER_GESTURE.DISLIKE_BY_USER
-		});
+    if(like_dish_array && like_dish_array.length > 0){
+      yield insertLikeDishByUser(user_id, like_dish_array);
+    }
+
+    if(dislike_dish_array && dislike_dish_array.length > 0){
+      yield insertDisLikeDishByUser(user_id, dislike_dish_array);
+    }
+
+    if(wishlist_dish_array && wishlist_dish_array.length > 0){
+      yield insertWishListDishByUser(user_id, wishlist_dish_array);
+    }
 		return ({
-			message: 'Dish photo dislike by user inserted succesfully',
-      user_gesture_id : createGesture.user_gesture_id
+			message: 'User\'s gestures on dish inserted succesfully'
 		});
 	}).then((userGestureMessage) => {
 		res.status(200)
@@ -67,239 +180,4 @@ exports.dislikePictureByUser = function (req, res) {
 			message: err.message
 		});
 	});
-};
-
-/**
- * @swagger
- * paths:
- *  /api/v1/userGesture/wishList:
- *    post:
- *      summary: Add dish in to user wish list.
- *      tags:
- *        - User Gestures
- *      description: Add wish list in to user as a JSON object
- *      consumes:
- *        - application/json
- *      parameters:
- *        - in: header
- *          name: Authorization
- *          description: an authorization header (Bearer eyJhbGciOiJI...)
- *          required: true
- *          type: string
- *        - in: body
- *          name: user gestures
- *          description: Set user gestures(wish list).
- *          schema:
- *            type: object
- *            required:
- *              - restaurant_dish_id
- *            properties:
- *              restaurant_dish_id:
- *                type: string
- *      responses:
- *        201:
- *          description: Created
- */
-
-exports.wishListPictureByUser = function (req, res) {
-  return co(function* () {
-    let user_id = req.decodedData.user_id;
-    let restaurant_dish_id = req.body.restaurant_dish_id;
-
-    let createGesture = yield db.userGestures.create({
-      user_id : user_id,
-      restaurant_dish_id : restaurant_dish_id,
-      gesture_type : CONSTANTS.USER_GESTURE.WISHLIST_OF_USER
-    });
-    return ({
-      message: 'Dish photo added in to user wishlist succesfully',
-      user_gesture_id : createGesture.user_gesture_id
-    });
-  }).then((userGestureMessage) => {
-    res.status(200)
-      .json(userGestureMessage);
-  }).catch((err) => {
-    res.status(400).json({
-      message: err.message
-    });
-  });
-};
-
-/**
- * @swagger
- * paths:
- *  /api/v1/userGesture/like:
- *    post:
- *      summary: set like dish by user.
- *      tags:
- *        - User Gestures
- *      description: set like dish by user as a JSON object
- *      consumes:
- *        - application/json
- *      parameters:
- *        - in: header
- *          name: Authorization
- *          description: an authorization header (Bearer eyJhbGciOiJI...)
- *          required: true
- *          type: string
- *        - in: body
- *          name: user gestures
- *          description: Set user gestures(like).
- *          schema:
- *            type: object
- *            required:
- *              - restaurant_dish_id
- *            properties:
- *              restaurant_dish_id:
- *                type: string
- *      responses:
- *        201:
- *          description: Created
- */
-
-exports.pictureLikeByUser = function (req, res) {
-  return co(function* () {
-    let user_id = req.decodedData.user_id;
-    let restaurant_dish_id = req.body.restaurant_dish_id;
-
-    let createGesture = yield db.userGestures.create({
-      user_id : user_id,
-      restaurant_dish_id : restaurant_dish_id,
-      gesture_type : CONSTANTS.USER_GESTURE.LIKE_BY_USER
-    });
-    return ({
-      message: 'Dish photo liked by user inserted succesfully',
-      user_gesture_id : createGesture.user_gesture_id
-    });
-  }).then((userGestureMessage) => {
-    res.status(200)
-      .json(userGestureMessage);
-  }).catch((err) => {
-    res.status(400).json({
-      message: err.message
-    });
-  });
-};
-
-function findDislikeDishesPics(user_id) {
-  return co(function* () {
-    let findDislikePictures = yield db.userGestures.findAll({
-      where : {
-        user_id : user_id,
-        gesture_type : CONSTANTS.USER_GESTURE.DISLIKE_BY_USER,
-        status : CONSTANTS.DB.STATUS.ACTIVE
-      },
-      attributes : ['user_gesture_id', 'restaurant_dish_id'],
-      order: 'created_at DESC'
-    });
-
-    return Promise.resolve(findDislikePictures);
-  }).catch((err) => {
-    return err;
-  });
-}
-
-function findDishInformation(restaurant_dish_id) {
-  return co(function* () {
-    let findDishInfo = yield db.restaurantDish.find({
-      attributes : ['restaurant_dish_id', 'dish_image_url','restaurant_info_id'],
-      where : {
-        restaurant_dish_id : restaurant_dish_id
-      },
-      include : [{
-        model : db.restaurantInfo,
-        attributes : ['restaurant_name', 'location_lat', 'location_long', 'restaurant_price']
-      }]
-    });
-    return Promise.resolve(findDishInfo);
-  }).catch((err) => {
-    return err;
-  });
-}
-
-/**
- * @swagger
- * definition:
- *   dislikeDishInfo:
- *     type: object
- *     properties:
- *       restaurant_info_id:
- *         type: string
- *       restaurant_name:
- *         type: string
- *       location_lat:
- *         type: number
- *       location_long:
- *         type: number
- *       restaurant_price:
- *         type: number
- *       restaurantDish:
- *         type: object
- *         properties:
- *           restaurant_dish_id:
- *             type: string
- *           dish_image_url:
- *             type: string
- */
-
-/**
- * @swagger
- * /api/v1/userGesture/undo:
- *   get:
- *     summary: Get dish information which is last dislike by user
- *     description: Get dish information as an JSON Object
- *     tags:
- *       - User Gestures
- *     produces:
- *       - application/json
- *     parameters:
- *       - in: header
- *         name: Authorization
- *         description: an authorization header (Bearer eyJhbGciOiJI...)
- *         type: string
- *         required : true
- *     responses:
- *       200:
- *         description: "successful operation"
- *         schema:
- *           type: object
- *           "$ref": "#/definitions/dislikeDishInfo"
- */
-
-exports.undoDislikePictureByUser = function (req, res) {
-  return co(function* () {
-    let user_id = req.decodedData.user_id;
-    let findDislikePictures = yield findDislikeDishesPics(user_id);
-
-    if(findDislikePictures.length > 1){
-      let restaurantDishInfo = yield findDishInformation(findDislikePictures[0].restaurant_dish_id);
-
-      yield findDislikePictures[0].updateAttributes({
-        status : CONSTANTS.DB.STATUS.DELETE
-      });
-      return ({
-        restaurant_info_id : restaurantDishInfo.restaurant_info_id,
-        restaurant_name : restaurantDishInfo.restaurantInfo.restaurant_name,
-        location_lat : restaurantDishInfo.restaurantInfo.location_lat,
-        location_long : restaurantDishInfo.restaurantInfo.location_long,
-        restaurant_price : restaurantDishInfo.restaurantInfo.restaurant_price,
-        restaurantDish : {
-          restaurant_dish_id : restaurantDishInfo.restaurant_dish_id,
-          dish_image_url : restaurantDishInfo.dish_image_url
-        }
-      });
-    } else {
-      return ({
-        message : 'There is nothing to undo'
-      })
-    }
-
-  }).then((userUnlikePicture) => {
-    res.status(200)
-      .json(userUnlikePicture);
-  }).catch((err) => {
-    res.status(400).json({
-      message: err.message
-    });
-  });
 };
