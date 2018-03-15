e6de8d134d3bc137411aa69c508c06947aae2892
nikeshrathod <niksrathod009@gmail.com>
2018-03-15 16:09:15 +0530

    Create model to save user review of text,audio and rating

---
+++ server/models/user_review.model.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = function (sequelize, DataTypes) {
+  const userReview = sequelize.define('userReview', {
+
+    user_review_id: {
+      type:  DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    restaurant_dish_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references : {
+        model : 'restaurantDish',
+        key : 'restaurant_dish_id',
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      }
+    },
+    audio_review_url : {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    text_review : {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    rating : {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    timestamps: true,
+    paranoid: true,
+    underscored: true,
+    freezeTableName:true,
+    tableName:'userReview'
+  });
+
+  return userReview;
+};
